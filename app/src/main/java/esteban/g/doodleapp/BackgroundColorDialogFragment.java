222eccaaadf61bb984074c8b4fbdcd688730378b
32TEBAN <estebanguanoluisa2002@gmail.com>
2023-12-12 16:51:03 -0500

    v0

---
+++ app/src/main/java/esteban/g/doodleapp/BackgroundColorDialogFragment.java
@@ -0,0 +1,18 @@
+package esteban.g.doodleapp;
+
+public class BackgroundColorDialogFragment extends ColorDialogFragment {
+    @Override
+    protected int getTitle() {
+        return R.string.title_color_dialog;
+    }
+
+    @Override
+    protected int getColorDoodleView(DoodleView doodleView){
+        return doodleView.getDrawingBackgroundColor();
+    }
+
+    @Override
+    protected void setColorDoodleView(DoodleView doodleView) {
+        doodleView.setDrawingBackgroundColor(color);
+    }
+}
---
+++ app/src/main/java/esteban/g/doodleapp/MainActivity.java
@@ -0,0 +1,41 @@
+package esteban.g.doodleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.Dialog;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            // determine screen size
+            int screenSize =
+                    getResources().getConfiguration().screenLayout &
+                            Configuration.SCREENLAYOUT_SIZE_MASK;
+
+            // use landscape for extra large tablets; otherwise, use portrait
+            if (screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+                setRequestedOrientation(
+                        ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            else
+                setRequestedOrientation(
+                        ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
